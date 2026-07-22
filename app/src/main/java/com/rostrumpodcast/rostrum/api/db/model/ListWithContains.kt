cc17dbe968f5a514b046db68f600e0cf7a0763ca
frostre1997 <n9043395@gmail.com>
2026-07-22 16:22:30 +0200

    Use @Embedded in ListWithContains to fix Room mapping

--- app/src/main/java/com/rostrumpodcast/rostrum/api/db/model/ListWithContains.kt
+++ app/src/main/java/com/rostrumpodcast/rostrum/api/db/model/ListWithContains.kt
@@ -1,6 +1,9 @@
 package com.rostrumpodcast.rostrum.api.db.model
 
+import androidx.room.Embedded
+
 data class ListWithContains(
-    val list: ListModel,
+    @Embedded val list: ListModel,   // Tell Room to map all ListModel fields from the query
     val contains: Boolean
 )
+
