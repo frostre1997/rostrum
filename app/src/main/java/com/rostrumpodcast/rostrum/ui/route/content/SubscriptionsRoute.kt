db94c279ec4f314e1feaf1434bbfac666c25343a
Frostre_ <n9043395@gmail.com>
2026-08-02 09:26:58 +0200

    Fix onClick reference in SubscriptionCard

--- app/src/main/java/com/rostrumpodcast/rostrum/ui/route/content/SubscriptionsRoute.kt
+++ app/src/main/java/com/rostrumpodcast/rostrum/ui/route/content/SubscriptionsRoute.kt
@@ -68,7 +68,7 @@ fun SubscriptionsRoute(
 
                 SubscriptionCard(
                     bundle = item,
-                    onClick = { onClickPodcast(item.subscription.podcast) }
+                    onClick = { onClickPodcast(item.it.podcast) }
                 )
             }
 
