8d8243561ce065e406ac07ab134f80726c46c176
Frostre_ <n9043395@gmail.com>
2026-08-02 09:42:08 +0200

    Refactor PodcastSettingsBottomSheet to placeholder
    Removed the existing implementation of the podcast settings bottom sheet and added a placeholder function.

--- app/src/main/java/com/rostrumpodcast/rostrum/ui/dialog/bottomsheet/PodcastSettingsBottomSheet.kt
+++ app/src/main/java/com/rostrumpodcast/rostrum/ui/dialog/bottomsheet/PodcastSettingsBottomSheet.kt
@@ -1,44 +1,8 @@
 package com.rostrumpodcast.rostrum.ui.dialog.bottomsheet
 
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import com.rostrumpodcast.rostrum.api.db.model.PodcastModel
-// import com.rostrumpodcast.rostrum.ui.view.model.podcastSettingsView
-import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun podcastettingsBottomSheet(
-    onDismiss: () -> Unit,
-    podcast: PodcastModel
-) {
-    val scope = rememberCoroutineScope()
-
-    val sheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = true
-    )
-
-    ModalBottomSheet(
-        onDismissRequest = {
-            onDismiss()
-        },
-
-        sheetState = sheetState,
-        contentWindowInsets = { WindowInsets() },
-        dragHandle = null,
-    ) {
-        // podcastSettingsView(
-            podcast = podcast,
-            onBack = {
-                scope.launch {
-                    sheetState.hide()
-                    onDismiss()
-                }
-            }
-        )
-    }
+fun PodcastSettingsBottomSheet() {
+    // TODO: Implement podcast settings
 }
